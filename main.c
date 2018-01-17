660782d08fa6dead6b77e5df29c47267f09f4dce
hans <hans15280248256@gmail.com>
2018-01-17 22:25:46 +0800

    update main.c

--- main.c
+++ main.c
@@ -15,12 +15,56 @@
 #include "touch.h"
 #include "album.h"
 #include "bmp.h"
-
+#include "piclib.h"
+#include "string.h"
 
 __inline void render_main_menu(void);
 __inline void render_side_bar(void);
 int pixel_count = 76800;
+
+//得到path路径下,目标文件的总个数
+//path:路径		    
+//返回值:总有效文件数
+u16 pic_get_tnum(u8 *path)
+{	  
+	u8 res;
+	u16 rval=0;
+ 	DIR tdir;	 		//临时目录
+	FILINFO tfileinfo;	//临时文件信息	
+	u8 *fn;	 			 			   			     
+    res=f_opendir(&tdir,(const TCHAR*)path); 	//打开目录
+  	tfileinfo.lfsize=_MAX_LFN*2+1;				//长文件名最大长度
+	tfileinfo.lfname=mymalloc(tfileinfo.lfsize);//为长文件缓存区分配内存
+	if(res==FR_OK&&tfileinfo.lfname!=NULL)
+	{
+		while(1)//查询总的有效文件数
+		{
+	        res=f_readdir(&tdir,&tfileinfo);       		//读取目录下的一个文件
+	        if(res!=FR_OK||tfileinfo.fname[0]==0)break;	//错误了/到末尾了,退出		  
+     		fn=(u8*)(*tfileinfo.lfname?tfileinfo.lfname:tfileinfo.fname);			 
+			res=f_typetell(fn);	
+			if((res&0XF0)==0X50)//取高四位,看看是不是图片文件	
+			{
+				rval++;//有效文件数增加1
+			}	    
+		}  
+	} 
+	return rval;
+}
+
+
 int main(void) {
+    DIR picdir;	 		//图片目录
+    FILINFO picfileinfo;//文件信息
+    u8 *fn;   			//长文件名
+	u8 *pname;			//带路径的文件名
+	u16 totpicnum; 		//图片文件总数
+    u16 curindex;		//图片当前索引
+    u8 pause=0;			//暂停标记
+	u16 *picindextbl;	//图片索引表
+    u8 t;
+    u16 temp;
+
     u16 mode;
 	u8 res;
     u8 option = 0;
@@ -49,16 +93,16 @@ int main(void) {
         LCD_Fill(60, 150, 240, 150 + 16, WHITE); //清除显示			  
         delay_ms(200);
     }
-    mem_init();		
+    mem_init();		//初始化内部内存池
     exfuns_init();
-    f_mount(fs, "0:", 1); //挂载SD卡 
+    f_mount(fs, "0:", 1); //挂载SD卡
     res=f_mkdir("0:/DCIM");
 		if(res!=FR_EXIST&&res!=FR_OK) 	//发生了错误
 	{		    
-				LCD_Clear(WHITE);     
+		LCD_Clear(WHITE);     
         LCD_ShowString(60, 150, 200, 16, 16, "Folder Error!");
-		   delay_ms(2000);
-return 0;		
+		delay_ms(2000);
+        return 0;		
 	}
 	
     EXTI15_Init(); //使能定时器捕获
@@ -86,11 +130,8 @@ return 0;
             } else if (tp_dev.x[0] <= 170 && tp_dev.x[0] >= 70 && tp_dev.y[0] <= 240 && tp_dev.y[0] >= 180) {
                 option = 2;
                 LCD_Fill(70, 180, 170, 240, LIGHTBLUE);
-
                 LCD_ShowString(95, 200, 100, 16, 16, "Camera");
-
             }
-
         }
     }
     if (option == 2) {
@@ -156,16 +197,118 @@ return 0;
 
     } else if (option == 1) {
         delay_ms(500);
-			            BACK_COLOR=WHITE;
+		BACK_COLOR=WHITE;
 
         // album
-        LCD_Clear(WHITE);
+        /*LCD_Clear(WHITE);
         LCD_ShowString(60, 150, 200, 16, 16, "Not implemented!");
         delay_ms(1000);
-        goto INIT;
-
+        goto INIT;*/
+        while(f_opendir(&picdir,"0:/DCIM"))//打开图片文件夹
+        {	    
+           LCD_ShowString(60,170,240,16,16,"图片文件夹错误!");
+           delay_ms(200);				  
+           LCD_Fill(60,170,240,186,WHITE);//清除显示	     
+           delay_ms(200);				  
+        }
+        totpicnum=pic_get_tnum("0:/DCIM"); //得到总有效文件数
+        while(totpicnum==NULL)//图片文件为0		
+        {	    
+           LCD_ShowString(60,170,240,16,16,"no picture file!");
+           delay_ms(200);				  
+           LCD_Fill(60,170,240,186,WHITE);//清除显示	     
+           delay_ms(200);				  
+        }
+        picfileinfo.lfsize=_MAX_LFN*2+1;						//长文件名最大长度
+        picfileinfo.lfname=mymalloc(picfileinfo.lfsize);	//为长文件缓存区分配内存
+        pname=mymalloc(picfileinfo.lfsize);				//为带路径的文件名分配内存
+        picindextbl=mymalloc(2*totpicnum);				//申请2*totpicnum个字节的内存,用于存放图片索引
+        while(picfileinfo.lfname==NULL||pname==NULL||picindextbl==NULL)//内存分配出错
+        {	    
+           LCD_ShowString(60,170,240,16,16,"memory allocation failure!");
+           delay_ms(200);				  
+           LCD_Fill(60,170,240,186,WHITE);//清除显示	     
+           delay_ms(200);				  
+        }
+        //记录索引
+        res=f_opendir(&picdir,"0:/DCIM"); //打开目录
+        if(res==FR_OK)
+        {
+            curindex=0;//当前索引为0
+            while(1)//全部查询一遍
+            {
+                temp=picdir.index;								//记录当前index
+                res=f_readdir(&picdir,&picfileinfo);       		//读取目录下的一个文件
+                if(res!=FR_OK||picfileinfo.fname[0]==0)break;	//错误了/到末尾了,退出		  
+                fn=(u8*)(*picfileinfo.lfname?picfileinfo.lfname:picfileinfo.fname);			 
+                res=f_typetell(fn);	
+                if((res&0XF0)==0X50)//取高四位,看看是不是图片文件	
+                {
+                    picindextbl[curindex]=temp;//记录索引
+                    curindex++;
+                }	    
+            } 
+        }
+        delay_ms(1500);
+        piclib_init();										//初始化画图	   	   
+        curindex=0;											//从0开始显示
+        res=f_opendir(&picdir,(const TCHAR*)"0:/DCIM"); 	//打开目录
+        while(res==FR_OK)//打开成功
+        {	
+            dir_sdi(&picdir,picindextbl[curindex]);			//改变当前目录索引	   
+            res=f_readdir(&picdir,&picfileinfo);       		//读取目录下的一个文件
+            if(res!=FR_OK||picfileinfo.fname[0]==0)break;	//错误了/到末尾了,退出
+            fn=(u8*)(*picfileinfo.lfname?picfileinfo.lfname:picfileinfo.fname);			 
+            strcpy((char*)pname,"0:/DCIM/");				//复制路径(目录)
+            strcat((char*)pname,(const char*)fn);  			//将文件名接在后面
+            LCD_Clear(BLACK);
+            ai_load_picfile(pname,0,0,lcddev.width,lcddev.height,1);//显示图片    
+            LCD_ShowString(2,2,240,16,16,pname); 				//显示图片名字
+            t=0;			
+            while(1) 
+            {
+				if(t>250){//下一张图片
+					curindex++;		   	
+					if(curindex>=totpicnum)curindex=0;//到末尾的时候,自动从头开始
+					break;
+				}
+				if((t%20)==0)LED0=!LED0;//LED0闪烁,提示程序正在运行.
+				tp_dev.scan(0);
+				if(tp_dev.sta & TP_PRES_DOWN) {
+					if(tp_dev.y[0] <= 50 && tp_dev.x[0] <= 50) // go back
+					{ 
+						goto INIT;
+					}else if(tp_dev.x[0] <= 50 && tp_dev.y[0] >= lcddev.height-50) //触摸左下角，删除窗口
+					{
+						f_unlink(pname);
+						LCD_ShowString(60, 150, 200, 16, 16, "Deleted!");
+						delay_ms(500);
+						break;
+					}else if(tp_dev.y[0] < 120) //触摸上部分的1/3，上一张
+					{
+						if(curindex)curindex--;
+						else curindex=totpicnum-1;
+						break;
+					}else if(tp_dev.y[0] > 120 && tp_dev.y[0]<240) //触摸中间的1/3，暂停
+					{
+						pause=!pause;
+						LED1=!pause;	//暂停的时候LED1亮.
+					}else if(tp_dev.y[0]>240)  //触摸下面的1/3，下一张
+					{
+						curindex++;		   	
+						if(curindex>=totpicnum)curindex=0;//到末尾的时候,自动从头开始
+						break;
+					}
+				}
+				if(pause==0)t++;
+				delay_ms(10);
+            }					    
+            res=0;  
+        }
+        myfree(picfileinfo.lfname);	//释放内存			    
+        myfree(pname);				//释放内存			    
+        myfree(picindextbl);		//释放内存
     }
-
 }
 
 __inline void render_main_menu(void) {
